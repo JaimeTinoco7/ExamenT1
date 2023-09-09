cb7e8c2267723088da1de95d2f47d7db87741bf9
JaimeTinoco7 <142696839+JaimeTinoco7@users.noreply.github.com>
2023-09-09 13:15:21 -0500

    Add files via upload

---
+++ src/exament1/ExamenT1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exament1;
+
+/**
+ *
+ * @author jaime
+ */
+public class ExamenT1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       FrameLista pantalla = new FrameLista();
+                pantalla.setLocationRelativeTo(null);
+                pantalla.setVisible(true);
+    }
+    
+}
